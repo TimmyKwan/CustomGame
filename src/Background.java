6718e1c560a20c17a2fcd888214f2dee6080d7a8
student <timothy_kwan@student.wayland.k12.ma.us>
2018-05-30 12:02:18 -0400

    Green Background

---
+++ src/Background.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Background {
+    public void draw(Graphics2D g2){
+        g2.setColor(Color.green);
+        g2.fillRect(0,0,1600,800);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,9 @@ public class Main extends JPanel {
         System.out.println("plz");
 
 
-        JFrame frame = new JFrame("Game of Life");
+        JFrame frame = new JFrame("CustomGame");
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        int width = 800;
+        int width = 1598;
         int height = 800;
         frame.setPreferredSize(new Dimension(width, height + 24));
 
@@ -26,7 +26,11 @@ public class Main extends JPanel {
         frame.setVisible(true);
 
     }
-    public void test(){
+    public void paintComponent(Graphics g){
+        Graphics2D g2 = (Graphics2D) g;
 
+        Background b = new Background();
+
+        b.draw(g2);
     }
 }
