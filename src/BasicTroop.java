61d6603da88c323be5446d2e72445a64f8bf83bb
yuding_ma <yuding_ma@student.wayland.k12.ma.us>
2018-06-07 14:07:51 -0400

    BasicTroop Class implemented, move method completed

--- src/BasicTroop.java
+++ src/BasicTroop.java
@@ -1,22 +1,41 @@
 import java.awt.*;
 
 public class BasicTroop {
-    private int x,y,vx,vy;
-    private Point destin;
+
+    private int health, dmg, moveRange, troopSize;
+    double x,y, vx, vy, distTravelled;
+    private Point initial, destin;
+    private boolean isMoveTurn;
     private int storage;
 
-    public BasicTroop(int x,int y){
+    public BasicTroop(int x, int y){
         this.x = x;
         this.y = y;
-        vx = 5;
-        vy = 5;
+        vx = 0;
+        vy = 0;
         destin = new Point(x,y);
         storage = 0;
+        initial = new Point(x,y);
+
+        troopSize = 10;
+        health = 6;
+        dmg = 2;
+        moveRange = 80;
+        distTravelled = 0;
+
+        //for testing purposes, is on true, should be initially false in actual game
+        isMoveTurn = true;
     }
 
     public void draw(Graphics2D g2){
+        g2.setColor(Color.RED);
         g2.setColor(Color.black);
-        g2.fillRect(x,y,10,10);
+        g2.fillRect((int)x - troopSize/2,(int)y - troopSize/2,troopSize,troopSize);
+
+        if(isMoveTurn) {
+            g2.setColor(new Color(255, 237, 0, 100));
+            g2.fillOval((int) x + (int) distTravelled - moveRange, (int) y + (int) distTravelled - moveRange, 2 * (moveRange - (int) distTravelled), 2 * (moveRange - (int) distTravelled));
+        }
     }
 
     public void adStorage(int amt, Platmium plat){
@@ -27,23 +46,68 @@ public class BasicTroop {
     }
 
     public void move(){
-        if (Math.abs(destin.getX()-x)>5 && x < destin.getX())
-            x += vx;
-        if (Math.abs(destin.getX()-x)>5 && x > destin.getX())
-            x -= vx;
-        if (Math.abs(destin.getY()-y)>5 && y < destin.getY())
-            y += vy;
-        if (Math.abs(destin.getY()-y)>5 && y > destin.getY())
-            y -= vy;
+
+        if(distTravelled <= moveRange){
+
+            //checks to see if the BasicTroop is moving or not, if not, distTravelled will not increase
+                //has a margin of error implemented
+            if(isMoveTurn && (destin.x > (int)x+1 || destin.x < (int)x-1) && (destin.y > (int)y+1 || destin.y < (int)y-1)){
+
+                //maybe make a recursion statement to lower vx and vy
+                vx = Math.abs(destin.x - initial.x) / 60.0;
+                vy = Math.abs(destin.y - initial.y) / 60.0;
+
+                if (x < destin.x)
+                    x += vx;
+                if (x > destin.x)
+                    x -= vx;
+                if (y < destin.y)
+                    y += vy;
+                if (y > destin.y)
+                    y -= vy;
+
+                distTravelled += Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
+            }
+        }
+        else{
+            setDestin((int)x,(int)y);
+            distTravelled = 0;
+            isMoveTurn = false;
+        }
     }
 
-    public void setVx(int vx) {
-        this.vx = vx;
+    //use in Main class, sets the initial position of the troop(where it had started to move)
+    public void setInitial(){
+        initial.setLocation((int)x,(int)y);
     }
-    public void setVy(int vy) {
-        this.vy = vy;
-    }
+
+    //use in Main class with MousePressed -- sets where the troop is headed to
     public void setDestin(int x,int y) {
         destin.setLocation(x, y);
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+    public void setDmg(int dmg){
+        this.dmg = dmg;
+    }
+    public void setMoveRange(int moveRange){
+        this.moveRange = moveRange;
+    }
+    //just in case you want to change troop size, although, with images, this wont be necessary
+    public void setTroopSize(int troopSize){
+        this.troopSize = troopSize;
+    }
+
+    //use setMoveTurn to set isMoveTurn to true when it is this troops turn
+    public void setMoveTurn(boolean isMoveTurn){
+        this.isMoveTurn = isMoveTurn;
+    }
+
+    public boolean isAlive(){
+        if(health > 0){
+            return true;
+        }
+        return false;
     }
 }
